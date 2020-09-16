67bbd9e48c7862fc1f7fe5a61424d130facf9c03
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-16 16:57:56 +0900

    Judge 修正

--- src/com/company/Judge.java
+++ src/com/company/Judge.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.util.*;
 
+import static com.company.GameMsg.*;
+
 /**
  * 勝敗判定を行うクラス
  */
@@ -19,38 +21,38 @@ public class Judge {
         int num = 2;
         // プレイヤーがバーストした場合、ディーラーの勝ち
         if(playerPoint == -1) {
-            System.out.println("ディーラーの勝ち");
+            showMessage(MSG_DEALERWIN);
             num = 1;
         }
         // プレイヤーの合計点数がディーラーの合計点数よりも大きい場合、プレイヤーの勝ち
         else if(playerPoint > dealerPoint) {
-            System.out.println("プレイヤーの勝ち");
+            showMessage(MSG_PLAYERWIN);
             num = 0;
         }
         // プレイヤーの合計点数がディーラーの合計点数よりも小さい場合、ディーラーの勝ち
         else if(playerPoint < dealerPoint) {
-            System.out.println("ディーラーの勝ち");
+            showMessage(MSG_DEALERWIN);
             num = 1;
         }
         // プレイヤーの合計点数が21、かつ、プレイヤーとディーラーの合計点数が同じ場合、(両方ブラックジャック)カードの枚数で勝敗判定する
         else if(playerPoint == 21 && playerPoint == dealerPoint) {
             // プレイヤーのカード枚数がディーラーのカード枚数より多い場合、ディーラーの勝ち
             if(playerCardList.size() > dealerCardList.size()) {
-                System.out.println("ディーラーの勝ち");
+                showMessage(MSG_DEALERWIN);
                 num = 1;
             }
             // プレイヤーのカード枚数がディーラーのカード枚数より少ない場合、プレイヤーの勝ち
             else if(playerCardList.size() < dealerCardList.size()) {
-                System.out.println("プレイヤーの勝ち");
+                showMessage(MSG_PLAYERWIN);
                 num = 0;
             }
             else {
-                System.out.println("引き分け");
+                showMessage(MSG_DRAW);
                 num = 2;
             }
         }
         else {
-            System.out.println("引き分け");
+            showMessage(MSG_DRAW);
             num = 2;
         }
         return num;
