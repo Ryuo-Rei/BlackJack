d92a54750f6da6f41a73b1cb2f8b170df0d6e6de
Ryuo-Rei <70551513+Ryuo-Rei@users.noreply.github.com>
2020-09-16 16:57:29 +0900

    GameMsg 修正

--- src/com/company/GameMsg.java
+++ src/com/company/GameMsg.java
@@ -11,13 +11,19 @@ public class GameMsg {
     final static String MSG_PLAYER = "プレイヤー";
     final static String MSG_DEALER = "ディーラー";
     final static String MSG_START = "ゲームを開始します。\r\n";
-    final static String MSG_BET_TRUE = "betするポイントを入力してください：";
-    final static String MSG_BET_FALSE = "入力された値ではbetできません。もう一度入力してください：";
-    final static String MSG_POINT = "現在の所持ポイント：" + Game.point + "\r\n";
+    final static String MSG_BET_TRUE = "betするポイントを入力してください(1 ~ 100)：";
+    final static String MSG_BET_FALSE = "入力された値ではbetできません。もう一度入力してください(1 ~ 100)：";
     final static String MSG_DRAWCARD = "カードを引きますか？(y:はい n:いいえ)：";
     final static String MSG_DRAWCARD_AGAIN = "もう一度カードを引きますか？(y:はい n:いいえ)：";
     final static String MSG_YESORNO = "y(Y)かn(N)を入力してください。\r\n";
     final static String MSG_BURST = "バーストしました。\r\n";
+    final static String MSG_SPADEACE = "S1";
+    final static String MSG_END_WIN = "プレイヤーの所持ポイントが2000点に達したため、終了 \r\n";
+    final static String MSG_END_LOSE = "プレイヤーの所持ポイントが無くなったため、終了 \r\n";
+    final static String MSG_PLAYERWIN = "プレイヤーの勝ち \r\n";
+    final static String MSG_DEALERWIN = "ディーラーの勝ち \r\n";
+    final static String MSG_DRAW = "引き分け \r\n";
+    final static String MSG_POINT = "現在の所持ポイント：";
 
     /**
      * メッセージを表示するメソッド
